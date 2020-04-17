e2cd9bb5628d873b18138ed6d1acc82261769d65
ozanne thomas <ozannethomasplay@gmail.com>
2020-04-17 15:12:58 +0200

    Handle mail factory error

--- libs/email-factory/src/modules/providers/email-factory.service.ts
+++ libs/email-factory/src/modules/providers/email-factory.service.ts
@@ -4,6 +4,9 @@ import { EmailCreator } from "../email-creator"
 import { ResetPasswordContext, TemplateResetPassword } from "../templates-maker"
 import { EmailAuthor } from "../author"
 
+const INVALID_LOGIN_ERROR_INTERNAL = "Invalid login: Username and Password not accepted, or may don't allow Less secure app, check this for more detail: https://support.google.com/mail/?p=BadCredentials"
+const INVALID_LOGIN_ERROR_CLIENT = "Email cannot be send due to internal error"
+
 type UserEmail = string
 interface EmailSender {
     user: string
@@ -17,6 +20,11 @@ export class EmailFactoryService {
         const templateResetEmail = new TemplateResetPassword(context)
         const emailCreator = new EmailCreator(emailAuthor, templateResetEmail)
 
-        await emailCreator.sendEmail(to)
+        try {
+            await emailCreator.sendEmail(to)
+        } catch (error) {
+            console.error(INVALID_LOGIN_ERROR_INTERNAL)
+            throw new Error(INVALID_LOGIN_ERROR_CLIENT)
+        }
     }
 }
